1c5d86dc3c8a473520dcda07db8fe599094dbe3b
Mahmoud Ibrahim <a1309720@unet.univie.ac.at>
2017-03-30 17:40:22 +0200

    kmeans 3

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,7 +6,7 @@ import algorithms.Kmeans;
 public class Test {
 
     public static void main(String[] args) {
-        Kmeans kmeans = new Kmeans(3,10,2);
+        Kmeans kmeans = new Kmeans(5,100,3);
         kmeans.init();
         kmeans.calculate();
     }
